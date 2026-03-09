2380b8230ea35f2049ee2f209a65210a360c3603
Kunal Raut <rautkunal918@gmail.com>
2026-03-09 15:46:49 +0000

    Container config done

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountid = 144553
+let accounEmail = "kunal@google.com"
+var accountPassword = "12345"
+accountCity = "Wardha"
+
+//accountid = 2
+console.log(accountid)
+
+accounEmail = "kunal@gmail.com"
+accountPassword = "54321"
+accountCity = "Amravati"
+let accountState;
+
+console.table([accountid, accounEmail, accountPassword, accountCity, accountState])
+
+/*Prefer not to use var 
+Because of issue in block scope and functional scope */
+// document.write("kunal is the good guy");
+
+alert("kunal raut ")
