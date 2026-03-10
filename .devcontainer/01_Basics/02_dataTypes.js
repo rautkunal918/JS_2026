9e8d916acb0a83bd109a4c1cfaae29aa8553c840
Kunal Raut <rautkunal918@gmail.com>
2026-03-10 21:15:28 +0000

    datatypes update

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3 + 3) // we are using node js, not browser
+
+// various datatypes 
+
+let name = "kunal"
+let = 21;
+let isLoggedIn = "false"
+
+
+//number =>2pwr53
+//bigint
+//string=>""
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+//symbol =>unique
+//object 
+
+console.log(typeof name, age, ); //output will be string,number
