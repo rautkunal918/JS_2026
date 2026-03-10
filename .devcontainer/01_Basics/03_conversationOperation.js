6f0bf4da0e944f66ea2c96109e5a1801fdd852bd
Kunal Raut <rautkunal918@gmail.com>
2026-03-10 21:24:04 +0000

    push

---
+++ .devcontainer/01_Basics/03_conversationOperation.js
@@ -0,0 +1,25 @@
+/*let score = "22ad"
+let score = "kunal"
+let score = true
+let score = false
+let score = undefined
+*/
+// let score = null
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "22" > 22
+// "22abc" >NaN
+// true>1 ; false>0
+
+
+
+let isLoggedIn = "sadf"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
